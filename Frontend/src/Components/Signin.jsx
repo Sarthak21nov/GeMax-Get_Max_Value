1fbb73c42f434c33abe52dfb84bbfff44a51cfc5
Sarthak Yash Srivastava <sarthak.nov21@gmail.com>
2024-12-28 21:05:51 +0530

    Some Slight Changes

--- Frontend/src/Components/Signin.jsx
+++ Frontend/src/Components/Signin.jsx
@@ -34,11 +34,14 @@ function Signin() {
             {alert && <div className="alert alert-danger" role="alert">{alert}</div>}
             <i><p className='text-lg font-serif mt-4 ml-4'>E-Mail ID:</p></i>
             <input type='email' placeholder='Enter E-mail ID' value={Email} onChange={HandleEmailChange} className='p-2 border-2 border-solid rounded-full w-[90%] shadow-xl ml-2'/>
+
             <i><p className='text-lg font-serif mt-3 ml-4'>Password:</p></i>
             <input type='password' placeholder='Enter Password' value={Password} onChange={HandlePasswordChange} className='p-2 border-2 border-solid rounded-full w-[90%] shadow-xl ml-2'/>
+
             <div className='flex justify-center items-center'>
               <button type="submit" className="btn btn-dark mt-2 m-auto">Login</button>
             </div> 
+            
             <p className='text-center m-2'>Do not have an Account? <a href='/SignUp' className='text-blue-500 hover:underline'>Sign Up</a></p>   
         </form>
       </div>
--- Frontend/src/Components/Signup.jsx
+++ Frontend/src/Components/Signup.jsx
@@ -46,12 +46,16 @@ function Signup() {
           {alert && <div className="alert alert-danger" role="alert">{alert}</div>}
           <i><p className='mt-6 ml-6 text-lg font-serif'>Name: </p></i>
           <input type='text' placeholder='Enter your Name' className='p-2 border-2 border-solid rounded-full w-[90%] shadow-xl ml-5' value={Name} onChange={HandleNameChange}/>
+
           <i><p className='mt-6 ml-6 text-lg font-serif'>Phone Number: </p></i>
           <input type='text' placeholder='Enter your Phone Number' className='p-2 border-2 border-solid rounded-full w-[90%] shadow-xl ml-5' value={PhoneNumber} onChange={HandleNumberChange}/>
+
           <i><p className='mt-6 ml-6 text-lg font-serif'>Email-ID: </p></i>
           <input type='email' placeholder='Enter your E-Mail ID' className='p-2 border-2 border-solid rounded-full w-[90%] shadow-xl ml-5' value={Email} onChange={HandleEmailChange}/>
+
           <i><p className='mt-6 ml-6 text-lg font-serif'>Password: </p></i>
           <input type='password' placeholder='Enter your Password' className='p-2 border-2 border-solid rounded-full w-[90%] shadow-xl ml-5' value={Password} onChange={HandlePasswordChange}/>
+          
           <div className='flex justify-center items-center'>
             <button type="submit" className="btn btn-dark mt-4 mb-4 m-auto rounded-full">Sign Up</button>
           </div>
