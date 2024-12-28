723489b8f84b387b8c89984265d0cc67ac47e4ba
Sarthak Yash Srivastava <sarthak.nov21@gmail.com>
2024-12-28 21:41:07 +0530

    Changes made in New Product

--- Frontend/src/Components/AddNewCard.jsx
+++ Frontend/src/Components/AddNewCard.jsx
@@ -45,20 +45,32 @@ function AddNewCard() {
         <div className='sm:w-[40%] w-[80%] flex-row min-h-[400px] bg-slate-100 shadow-3xl rounded-lg p-2 glow'>
             {alert && <div className="alert alert-danger" role="alert">{alert}</div>}
             <div className='flex-row'>
-                <i><p className='font-serif text-xl pl-2'>Product Images: </p></i>
+                <i><p className='font-serif text-xl pl-2 mt-3'>Product Images: </p></i>
                 <input type='file' name='images' multiple accept='/*' onChange={HandleFileChange} className='p-2'/>
             </div>
             <div className='text-xl font-serif p-2'>
-                <i><p>Product Name: </p></i>
+                <i><p className='mt-3'>Product Name: </p></i>
                 <input type='text' value={ProdName} onChange={HandleNameChange} placeholder="Enter Product Name" className='w-full p-1 text-lg rounded-xl shadow-2xl'/>
-                <i><p>Product Price: </p></i>
+                <i><p className='mt-3'>Product Price: </p></i>
                 <div className='flex gap-6'>
                     <input type='number' value={Price} onChange={HandlePriceChange} placeholder="Enter Price" className='w-[40%] p-1 text-lg rounded-xl shadow-2xl'/>
                     <button type="button" className="btn btn-dark">Get Suggestion Using AI</button>
                 </div>
-                <i><p>Description: </p></i>
-                <textarea placeholder='Product Description' value={ProdDescription} onChange={HandleDescriptionChange} className='w-full min-h-[100px] p-2'/> 
-                <div className='flex gap-6'>
+                <i><p className='mt-3'>Description: </p></i>
+                <textarea placeholder='Product Description' value={ProdDescription} onChange={HandleDescriptionChange} className='w-full min-h-[100px] p-2'/>
+                <i><label htmlFor='dropdown' className='mt-3'>Choose an Option: </label></i>
+                <select id='dropdown' className='w-full p-2'>
+                    <option value=''>Select an Option</option>
+                    <option value='Electronics'>Electronics</option>
+                    <option value='Beauty'>Beauty</option>
+                    <option value='Essentials'>Essentials</option>
+                    <option value='Fashion'>Fashion</option>
+                    <option value='Home And Living'>Home and Living</option>
+                    <option value='Sports'>Sports</option>
+                    <option value='Stationery'>Stationery</option>
+                    <option value='Travel'>Travel</option>
+                </select>     
+                <div className='flex gap-6 mt-3'>
                     <i><p>Seller Name: </p></i>
                     <p>{SellerName}</p>
                 </div>
